e2f033da2c568645e07ab52d737bfe0a382617f1
Faishingw <632183339@qq.com>
2024-06-26 14:43:27 +0800

    升级nacos版本

--- 4-Finchley/alibaba-nacos-config-client/src/main/java/com/didispace/alibaba/nacos/config/client/TestApplication.java
+++ 4-Finchley/alibaba-nacos-config-client/src/main/java/com/didispace/alibaba/nacos/config/client/TestApplication.java
@@ -1,14 +1,22 @@
 package com.didispace.alibaba.nacos.config.client;
 
-import lombok.extern.slf4j.Slf4j;
+import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
+import static org.springframework.core.env.StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME;
+import static org.springframework.core.env.StandardEnvironment.SYSTEM_PROPERTIES_PROPERTY_SOURCE_NAME;
 
+@EnableNacosConfig
+@NacosPropertySource(name = "custom", dataId = "didispace", autoRefreshed = true, first = true, before = SYSTEM_PROPERTIES_PROPERTY_SOURCE_NAME, after = SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME)
 @SpringBootApplication
 public class TestApplication {
 
@@ -16,7 +24,32 @@ public class TestApplication {
         SpringApplication.run(TestApplication.class, args);
     }
 
-    @Slf4j
+    @RequestScope
+    @Component
+    class AB {
+        String a;
+
+        public void abc(String a) {
+            this.a = a;
+        }
+    }
+
+
+    @RestController
+    static class TestController1 {
+
+        @Autowired
+        private AB ab;
+
+        @GetMapping("/test1")
+        public String hello() {
+            ab.abc("a");
+            return ab.a;
+        }
+
+    }
+
+
     @RestController
     @RefreshScope
     static class TestController {
@@ -33,10 +66,9 @@ public class TestApplication {
 
     /**
      * 多文件加载的例子使用的验证接口
-     *
+     * <p>
      * blog: http://blog.didispace.com/spring-cloud-alibaba-nacos-config-3/
      */
-    @Slf4j
     @RestController
     @RefreshScope
     static class Test2Controller {
